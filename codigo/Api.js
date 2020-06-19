1543f0c4669d6d00344026eac361466039cebccf
FranSpinelli <franciscospinelli98@gmail.com>
2020-06-19 20:21:19 -0300

    solved data.json issue

--- codigo/Api.js
+++ codigo/Api.js
@@ -5,13 +5,14 @@ const Errores = require('./Errores.js');
 const errorHandler = require('./ApiErrorHandler.js');
 
 const port = 8080;
-const unqfy = funciones.getUNQfy();
+//const unqfy = funciones.getUNQfy();
 
 const express = require('express');
 const rootApp = express();
 const artists = express();
 const albums = express();
 const tracks = express();
+const playlists = express();
 
 rootApp.use(bodyParser.json());
 
@@ -21,7 +22,8 @@ rootApp.route('/api').get((req,res) => {
 
 artists.route('/artists')
 .get((req, res) => {
-
+    
+    let unqfy = funciones.getUNQfy();
     let artistas = []
     if (req.query.name){
         artistas = unqfy.searchByName(req.query.name).artists.map(artista => {    
@@ -36,6 +38,7 @@ artists.route('/artists')
 }).post((req,res) => {
 
     artistMissingValueChecking(req);
+    let unqfy = funciones.getUNQfy();
     let nuevoArtista = unqfy.addArtist(req.body);
     funciones.saveUNQfy(unqfy);
 
@@ -44,11 +47,13 @@ artists.route('/artists')
 
 artists.route('/artists/:id')
 .get((req, res) =>{
-
+    
+    let unqfy = funciones.getUNQfy();
     let artista = unqfy.getArtistaConID(parseInt(req.params.id));
     res.status(200).json(returnedArtist(artista));
 }).delete((req,res) => {
-
+    
+    let unqfy = funciones.getUNQfy();
     unqfy.eliminarArtista(parseInt(req.params.id));
     funciones.saveUNQfy(unqfy);
 
@@ -56,6 +61,7 @@ artists.route('/artists/:id')
 }).patch((req,res) => {
     
     artistMissingValueChecking(req);
+    let unqfy = funciones.getUNQfy();
     let artista = unqfy.getArtistaConID(parseInt(req.params.id));  
     
     artista.nombre = req.body.name;
@@ -66,6 +72,8 @@ artists.route('/artists/:id')
 })
 //--------------------------------------------------------------------------------------------------------------
 albums.route('/albums').get((req,res) => {
+    
+    let unqfy = funciones.getUNQfy();
     let albums = []
     if (req.query.name){
         albums = unqfy.searchByName(req.query.name).albums.map(album => {
@@ -80,10 +88,11 @@ albums.route('/albums').get((req,res) => {
 }).post((req,res) => {
     albumMissingValueChecking(req);
     try{
-    let nuevoAlbum = unqfy.addAlbum(req.body.artistId, req.body);
-    funciones.saveUNQfy(unqfy);
+        let unqfy = funciones.getUNQfy();
+        let nuevoAlbum = unqfy.addAlbum(req.body.artistId, req.body);
+        funciones.saveUNQfy(unqfy);
 
-    res.status(201).json(returnedAlbum(nuevoAlbum));
+        res.status(201).json(returnedAlbum(nuevoAlbum));
     }catch(err){
         if(err instanceof Errores.NoExisteElementoConID){
             throw new ErroresApi.InexistentRelatedSource;
@@ -95,13 +104,14 @@ albums.route('/albums').get((req,res) => {
 
 albums.route('/albums/:id')
 .get((req,res) => {
+    let unqfy = funciones.getUNQfy();
     let album = unqfy.getAlbumConID(parseInt(req.params.id));
 
     res.status(200).json(returnedAlbum(album));
 }).delete((req,res) => {
-    let albumID = parseInt(req.params.id);
 
-    unqfy.eliminarAlbum(albumID);
+    let unqfy = funciones.getUNQfy();
+    unqfy.eliminarAlbum(parseInt(req.params.id));
     funciones.saveUNQfy(unqfy);
 
     res.sendStatus(204);
@@ -110,6 +120,7 @@ albums.route('/albums/:id')
         throw new ErroresApi.MissingValue()
     }
 
+    let unqfy = funciones.getUNQfy();
     let album = unqfy.getAlbumConID(parseInt(req.params.id));    
     album.añoDeLanzamiento = req.body.year;
     funciones.saveUNQfy(unqfy);
@@ -120,6 +131,7 @@ albums.route('/albums/:id')
 tracks.route('/tracks/:id/lyrics')
 .get((req,res) => {
 
+    let unqfy = funciones.getUNQfy();
     let track = unqfy.getTrackConID(parseInt(req.params.id));
     unqfy.getTrackLyrics(parseInt(req.params.id)).then( response => {
         
@@ -135,7 +147,12 @@ tracks.route('/tracks/:id/lyrics')
     });
 })
 
-rootApp.use('/api', artists, albums, tracks,);
+playlists.route('/playlists')
+.get((req,res) => {
+    
+});
+
+rootApp.use('/api', artists, albums, tracks,playlists);
 rootApp.use(function(req,res){
     throw new ErroresApi.WrongRoute();
 });
